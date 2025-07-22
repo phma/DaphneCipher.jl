4b59a078dd24b386d0641683a26fb4d7d7b66307
Pierre Abbat <phma@bezitopo.org>
2025-07-22 03:01:58 -0400

    Add comments and docstrings

--- src/DaphneCipher.jl
+++ src/DaphneCipher.jl
@@ -1,3 +1,12 @@
+"""
+    DaphneCipher
+
+Daphne is a self-synchronizing byte stream cipher. It takes an arbitrary-length
+key and encrypts a stream of bytes in such a way that, if a byte is garbled,
+lost, or inserted in the ciphertext, the plaintext will be garbled for the key
+length plus a variable number of bytes, usually less than 256, and will then
+recover.
+"""
 module DaphneCipher
 using OffsetArrays
 export twist,funSbox,stepp,invStep,left,right
@@ -9,6 +18,15 @@ function twist(n::Integer,k::Integer)
   bitrotate(n,count_ones(n)*k)
 end
 
+# This is a P-box used to generate the S-box.
+# 1 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 1 0
+# 0 0 0 1 0 0 0 0
+# 0 0 0 0 0 0 0 1
+# 0 0 0 0 0 1 0 0
+# 0 0 1 0 0 0 0 0
+# 0 1 0 0 0 0 0 0
+# 0 0 0 0 1 0 0 0
 function shuffle(n::UInt8)
   bitrotate(n&0x54,3)|
   bitrotate(n&0x28,7)|
@@ -31,6 +49,8 @@ for i in 0x00:0xff
   invSbox[funSbox(i)]=i
 end
 
+# Equivalent to shifting m and n left by 1 with a 1 bit (thus making them odd),
+# multiplying, and shifting right (discarding the 1 bit).
 function mulOdd(a::Integer,b::Integer)
   a+b+0x2*a*b
 end
@@ -66,6 +86,12 @@ div257(m,n)=mul257(m,inv257[n])
 stepp(x,l,r)=mulOdd(sbox[mul257(x,l)],r)
 invStep(x,l,r)=div257(invSbox[divOdd(x,r)],l)
 
+"""
+    mutable struct Daphne
+
+Contains the key and state of a Daphne stream cipher.
+`Daphne()` creates a keyless Daphne; call `setKey!` to set the key.
+"""
 mutable struct Daphne
   key	::Vector{UInt8}
   sreg	::Vector{UInt8}
@@ -73,6 +99,12 @@ mutable struct Daphne
   Daphne()=new([],[],0)
 end
 
+"""
+    setKey!(d::Daphne,k::Vector{UInt8})
+
+Set the key of a Daphne. `k` should be at least 16 bytes, or 32 bytes to avoid
+Grover's algorithm, but longer keys make encryption and decryption slower.
+"""
 function setKey!(d::Daphne,k::Vector{UInt8})
   d.key=copy(k)
   d.sreg=zero(k)
@@ -95,6 +127,11 @@ function right(d::Daphne)
   a
 end
 
+"""
+    encrypt!(d::Daphne,plain::UInt8)
+
+Encrypt one byte. `d`'s state is changed.
+"""
 function encrypt!(d::Daphne,plain::UInt8)
   crypt=stepp(plain,left(d),right(d))
   d.acc+=plain
@@ -103,6 +140,11 @@ function encrypt!(d::Daphne,plain::UInt8)
   crypt
 end
 
+"""
+    decrypt!(d::Daphne,crypt::UInt8)
+
+Decrypt one byte. `d`'s state is changed.
+"""
 function decrypt!(d::Daphne,crypt::UInt8)
   plain=invStep(crypt,left(d),right(d))
   d.acc+=plain
@@ -111,6 +153,12 @@ function decrypt!(d::Daphne,crypt::UInt8)
   plain
 end
 
+"""
+    encrypt!(d::Daphne,plain::Vector{UInt8})
+
+Encrypt many bytes. `d` is changed, but `plain` is not; returns the encrypted bytes
+in a new `Vector`.
+"""
 function encrypt!(d::Daphne,plain::Vector{UInt8})
   crypt=UInt8[]
   sizehint!(crypt,length(plain))
@@ -120,6 +168,13 @@ function encrypt!(d::Daphne,plain::Vector{UInt8})
   crypt
 end
 
+
+"""
+    decrypt!(d::Daphne,crypt::Vector{UInt8})
+
+Decrypt many bytes. `d` is changed, but `crypt` is not; returns the decrypted bytes
+in a new `Vector`.
+"""
 function decrypt!(d::Daphne,crypt::Vector{UInt8})
   plain=UInt8[]
   sizehint!(plain,length(crypt))
